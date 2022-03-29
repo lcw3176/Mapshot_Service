73ad6a08d7e830f690d237751558bc96e1da2a4f
joebrooks <lcw3176@naver.com>
2022-03-29 16:20:05 +0900

    크롬 드라이버로 변경

---
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
@@ -0,0 +1,30 @@
+package com.joebrooks.mapshotimageapi.driver;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChromeDriverConfig {
+
+    @Bean
+    public ChromeOptions chromeOptions(){
+        String chromeDriverPath = "CHROMEDRIVER_PATH";
+        String chromeBinPath = "GOOGLE_CHROME_BIN";
+
+        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
+//        options.setBinary(System.getenv(chromeBinPath));
+
+        return options;
+    }
+
+    @Bean(destroyMethod = "quit")
+    public CustomChromeDriver chromeDriver() throws Exception {
+        return new CustomChromeDriver(chromeOptions());
+    }
+}
---
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/CustomChromeDriver.java
@@ -0,0 +1,55 @@
+package com.joebrooks.mapshotimageapi.driver;
+
+
+import com.google.common.collect.ImmutableMap;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CommandInfo;
+import org.openqa.selenium.remote.HttpCommandExecutor;
+import org.openqa.selenium.remote.http.HttpMethod;
+
+import java.lang.reflect.Method;
+import java.util.Base64;
+import java.util.Map;
+
+public class CustomChromeDriver extends ChromeDriver {
+
+    public CustomChromeDriver(ChromeOptions options) throws Exception {
+        this(ChromeDriverService.createDefaultService(), options);
+    }
+
+    public CustomChromeDriver(ChromeDriverService service, ChromeOptions options) throws Exception {
+        super(service, options);
+        CommandInfo cmd = new CommandInfo("/session/:sessionId/chromium/send_command_and_get_result", HttpMethod.POST);
+        Method defineCommand = HttpCommandExecutor.class.getDeclaredMethod("defineCommand", String.class, CommandInfo.class);
+        defineCommand.setAccessible(true);
+        defineCommand.invoke(super.getCommandExecutor(), "sendCommand", cmd);
+    }
+
+    public byte[] getFullScreenshot() {
+        Object metrics = sendEvaluate(
+                "({" +
+                        "width: Math.max(window.innerWidth,document.body.scrollWidth,document.documentElement.scrollWidth)|0," +
+                        "height: Math.max(window.innerHeight,document.body.scrollHeight,document.documentElement.scrollHeight)|0," +
+                        "deviceScaleFactor: window.devicePixelRatio || 1," +
+                        "mobile: typeof window.orientation !== 'undefined'" +
+                        "})");
+        sendCommand("Emulation.setDeviceMetricsOverride", metrics);
+        Object result = sendCommand("Page.captureScreenshot", ImmutableMap.of("format", "jpeg", "quality", 80));
+        sendCommand("Emulation.clearDeviceMetricsOverride", ImmutableMap.of());
+        String base64Encoded = (String)((Map<String, ?>)result).get("data");
+
+        return Base64.getDecoder().decode(base64Encoded);
+    }
+
+    protected Object sendCommand(String cmd, Object params) {
+        return execute("sendCommand", ImmutableMap.of("cmd", cmd, "params", params)).getValue();
+    }
+
+    protected Object sendEvaluate(String script) {
+        Object response = sendCommand("Runtime.evaluate", ImmutableMap.of("returnByValue", true, "expression", script));
+        Object result = ((Map<String, ?>)response).get("result");
+        return ((Map<String, ?>)result).get("value");
+    }
+}
