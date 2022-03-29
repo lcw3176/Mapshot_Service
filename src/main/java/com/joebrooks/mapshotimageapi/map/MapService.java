47bf2fb581020fd7008c4224b1b828f35f5116ed
joebrooks <lcw3176@naver.com>
2022-03-29 16:20:16 +0900

    임시 테스트

--- src/main/java/com/joebrooks/mapshotimageapi/map/MapService.java
+++ src/main/java/com/joebrooks/mapshotimageapi/map/MapService.java
@@ -1,5 +1,6 @@
 package com.joebrooks.mapshotimageapi.map;
 
+import com.joebrooks.mapshotimageapi.driver.CustomChromeDriver;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -12,13 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MapService {
 
-    private final FirefoxDriver firefoxDriver;
+    private final CustomChromeDriver chromeDriver;
     private final WebDriverWait waiter;
 
     public byte[] getImage(String url){
-        firefoxDriver.get(url);
-        waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("checker_true")));
+        chromeDriver.get("https://www.naver.com");
+//        waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("checker_true")));
 
-        return firefoxDriver.getFullPageScreenshotAs(OutputType.BYTES);
+        return chromeDriver.getFullScreenshot();
     }
 }
