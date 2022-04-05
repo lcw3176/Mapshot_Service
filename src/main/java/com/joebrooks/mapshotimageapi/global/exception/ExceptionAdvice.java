4822d93ee98d39c5cd49f5dc6bdc8a6dc97b06cc
joebrooks <lcw3176@naver.com>
2022-04-05 14:26:12 +0900

    예외 처리

---
+++ src/main/java/com/joebrooks/mapshotimageapi/global/exception/ExceptionAdvice.java
@@ -0,0 +1,44 @@
+package com.joebrooks.mapshotimageapi.global.exception;
+
+import com.joebrooks.mapshotimageapi.global.log.IMessageClient;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.ClientAbortException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchSessionException;
+import org.openqa.selenium.WebDriverException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ExceptionAdvice {
+
+    private final IMessageClient iMessageClient;
+
+
+    @ExceptionHandler(NoSuchSessionException.class)
+    public void clientDisconnectedHandler(){
+
+    }
+
+    @ExceptionHandler({ClientAbortException.class, WebDriverException.class})
+    public void serverTimeOutHandler(){
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void exceptionHandler(Exception e) {
+        int len = Math.min(ExceptionUtils.getStackTrace(e).length(), 1700);
+
+        ExceptionResponse errorMessage = ExceptionResponse.builder()
+                .name(e.getClass().toString())
+                .message(ExceptionUtils.getStackTrace(e).substring(0, len))
+                .build();
+
+        sendErrorMessage(errorMessage);
+    }
+
+    private void sendErrorMessage(ExceptionResponse error){
+        iMessageClient.sendMessage(error);
+    }
+}
