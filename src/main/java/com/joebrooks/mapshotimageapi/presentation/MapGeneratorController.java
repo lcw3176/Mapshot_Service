6979c6c01576cabe64dcd02011d2bfa8a960feab
joebrooks <lcw3176@naver.com>
2022-04-05 14:29:06 +0900

    MapGeneratorController

--- src/main/java/com/joebrooks/mapshotimageapi/presentation/MapGeneratorController.java
+++ src/main/java/com/joebrooks/mapshotimageapi/presentation/MapGeneratorController.java
@@ -1,6 +1,6 @@
 package com.joebrooks.mapshotimageapi.presentation;
 
-import com.joebrooks.mapshotimageapi.map.MapRequest;
+import com.joebrooks.mapshotimageapi.kakaoMap.KakaoMapRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/map/kakao/crawl")
-public class KakaoMapCrawlerController {
+@RequestMapping("/map/gen/kakao")
+public class MapGeneratorController {
 
     @GetMapping
-    public String getKakaoMap(@ModelAttribute MapRequest kakaoMap, Model model){
-        model.addAttribute("kakaoMapInfo", kakaoMap);
+    public String getKakaoMap(@ModelAttribute KakaoMapRequest mapRequest, Model model){
+        model.addAttribute("mapRequest", mapRequest);
 
         return "map/kakao";
     }
