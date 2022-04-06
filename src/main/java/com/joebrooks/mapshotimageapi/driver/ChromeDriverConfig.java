67fa18c6e958126320a39441da71cc103fa9827c
joebrooks <lcw3176@naver.com>
2022-04-06 14:21:38 +0900

    경로 변경

--- src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
@@ -12,13 +12,13 @@ public class ChromeDriverConfig {
         String chromeDriverPath = "CHROMEDRIVER_PATH";
         String chromeBinPath = "GOOGLE_CHROME_BIN";
 
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getenv(chromeDriverPath));
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
-//        options.setBinary(System.getenv(chromeBinPath));
+        options.setBinary(System.getenv(chromeBinPath));
 
         return options;
     }
