b0a64d7a347114fcecadedbb04d861e4a79083ad
joebrooks <lcw3176@naver.com>
2022-04-06 14:36:42 +0900

    바이너리 경로 제거

--- src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
@@ -10,7 +10,6 @@ public class ChromeDriverConfig {
     @Bean
     public ChromeOptions chromeOptions(){
         String chromeDriverPath = "CHROMEDRIVER_PATH";
-        String chromeBinPath = "GOOGLE_CHROME_BIN";
 
         System.setProperty("webdriver.chrome.driver", System.getenv(chromeDriverPath));
         ChromeOptions options = new ChromeOptions();
@@ -18,7 +17,6 @@ public class ChromeDriverConfig {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
-        options.setBinary(System.getenv(chromeBinPath));
 
         return options;
     }
