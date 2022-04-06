4e1e6b457360a51df5d33047a63058d42bbc2e86
joebrooks <lcw3176@naver.com>
2022-04-06 15:24:11 +0900

    navbar 추가

---
+++ src/main/resources/static/js/navbar.js
@@ -0,0 +1,9 @@
+navbarBurgerClick = function navbarBurgerClick(id) {
+    if (id.getAttribute("class") === "navbar-burger") {
+        id.setAttribute("class", "navbar-burger is-active");
+        document.getElementById("navbar-menu").setAttribute("class", "navbar-menu is-active");
+    } else {
+        id.setAttribute("class", "navbar-burger");
+        document.getElementById("navbar-menu").setAttribute("class", "navbar-menu");
+    }
+};
