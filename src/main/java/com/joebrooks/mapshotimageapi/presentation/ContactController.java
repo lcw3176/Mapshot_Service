894b27ebd567f546000439d1f9815ad9822485a1
joebrooks <lcw3176@naver.com>
2022-04-07 15:36:36 +0900

    contact 페이지

---
+++ src/main/java/com/joebrooks/mapshotimageapi/presentation/ContactController.java
@@ -0,0 +1,16 @@
+package com.joebrooks.mapshotimageapi.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contact")
+public class ContactController {
+
+    @GetMapping
+    public String showContact(){
+
+        return "contact";
+    }
+}
