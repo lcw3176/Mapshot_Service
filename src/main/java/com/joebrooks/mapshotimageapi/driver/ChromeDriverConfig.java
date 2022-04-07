411c38d9b1dfe0fe53b4b41aa5b32af3d8c0b9bb
joebrooks <lcw3176@naver.com>
2022-04-07 14:50:00 +0900

    포트 옵션 제거

--- src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
@@ -19,7 +19,6 @@ public class ChromeDriverConfig {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-debugging-port=9222");
         options.setBinary(System.getenv(chromeBinaryPath));
 
         return options;
