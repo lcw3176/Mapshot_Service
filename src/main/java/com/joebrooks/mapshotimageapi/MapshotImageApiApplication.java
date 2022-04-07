b42b02d3f0279c00fd5d62af9c93e6b66262dda9
Chanwoo Lee <lcw3176@naver.com>
2022-04-08 00:22:12 +0900

    Feature/db/구축 (#3)
    * db 제외
    
    * jpa 추가
    
    * jpa 설정 추가
    
    * createdDate 추가
    
    * patchNote Entity 생성
    
    * patchNote -> notice로 변경
    
    * ManualEntity
    
    * ManualEntity 제거

--- src/main/java/com/joebrooks/mapshotimageapi/MapshotImageApiApplication.java
+++ src/main/java/com/joebrooks/mapshotimageapi/MapshotImageApiApplication.java
@@ -2,8 +2,10 @@ package com.joebrooks.mapshotimageapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MapshotImageApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/joebrooks/mapshotimageapi/repository/notice/NoticeEntity.java
@@ -0,0 +1,32 @@
+package com.joebrooks.mapshotimageapi.repository.notice;
+
+import com.joebrooks.mapshotimageapi.repository.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "notice")
+public class NoticeEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private NoticeType noticeType;
+
+    @Column(name = "content")
+    private String content;
+
+}
