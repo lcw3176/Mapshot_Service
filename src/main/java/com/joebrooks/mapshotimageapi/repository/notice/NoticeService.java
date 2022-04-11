e8111cc2a67baa23f0df055698b4ada53a8cc7a9
joebrooks <lcw3176@naver.com>
2022-04-11 16:35:39 +0900

    notice db

---
+++ src/main/java/com/joebrooks/mapshotimageapi/repository/notice/NoticeService.java
@@ -0,0 +1,26 @@
+package com.joebrooks.mapshotimageapi.repository.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    public void addPost(NoticeEntity noticeEntity){
+        noticeRepository.save(noticeEntity);
+    }
+
+    public Page<NoticeEntity> getPosts(int index){
+        return noticeRepository.findAll(PageRequest.of(index, 10, Sort.by("id").descending()));
+    }
+}
