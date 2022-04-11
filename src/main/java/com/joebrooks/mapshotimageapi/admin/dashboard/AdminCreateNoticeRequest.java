024632b32ee3d6989a76f39ff00a5ec82f94d3c8
Chanwoo Lee <lcw3176@naver.com>
2022-04-11 17:02:13 +0900

    Feature/공지사항db연동 (#9)
    * notice db
    
    * valid 추가
    
    * 유효성 검증, 글 등록 로직 추가
    
    * 에러 메세지
    
    * 에러 메세지
    
    * datetime 포매팅
    
    * 공지사항 추가
    
    * 게시글 가져오기 추가

--- src/main/java/com/joebrooks/mapshotimageapi/admin/dashboard/AdminCreateNoticeRequest.java
+++ src/main/java/com/joebrooks/mapshotimageapi/admin/dashboard/AdminCreateNoticeRequest.java
@@ -4,11 +4,18 @@ import com.joebrooks.mapshotimageapi.repository.notice.NoticeType;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 public class AdminCreateNoticeRequest {
+
+    @NotEmpty(message = "제목을 입력해 주세요")
     private String title;
+
     private NoticeType noticeType;
+
+    @NotEmpty(message = "내용을 입력해 주세요")
     private String content;
 
 }
--- src/main/java/com/joebrooks/mapshotimageapi/admin/dashboard/AdminDashboardController.java
+++ src/main/java/com/joebrooks/mapshotimageapi/admin/dashboard/AdminDashboardController.java
@@ -1,19 +1,34 @@
 package com.joebrooks.mapshotimageapi.admin.dashboard;
 
+import com.joebrooks.mapshotimageapi.repository.notice.NoticeEntity;
+import com.joebrooks.mapshotimageapi.repository.notice.NoticeService;
 import com.joebrooks.mapshotimageapi.repository.notice.NoticeType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin/board")
 public class AdminDashboardController {
 
+    private final NoticeService noticeService;
+
     @GetMapping
     public String showAdminPage(Model model){
+        Page<NoticeEntity> posts = noticeService.getPosts(0);
+
         model.addAttribute("todayUser", 1200);
         model.addAttribute("totalUser", 24000);
+        model.addAttribute("posts", posts);
 
         return "admin-board";
     }
@@ -23,5 +38,29 @@ public class AdminDashboardController {
         model.addAttribute("noticeType", NoticeType.values());
 
         return "admin-create-post";
+    }
+
+    @PostMapping("/create")
+    public String registerPost(Model model, @Valid AdminCreateNoticeRequest noticeRequest, Errors errors){
+
+        if(errors.hasErrors()){
+            model.addAttribute("adminCreateNoticeRequest", noticeRequest);
+            model.addAttribute("noticeType", NoticeType.values());
+
+            for (FieldError error : errors.getFieldErrors()) {
+                String validKeyName = String.format("valid_%s", error.getField());
+                model.addAttribute(validKeyName, error.getDefaultMessage());
+            }
+
+            return "admin-create-post";
+        }
+
+        noticeService.addPost(NoticeEntity.builder()
+                .title(noticeRequest.getTitle())
+                .noticeType(noticeRequest.getNoticeType())
+                .content(noticeRequest.getContent())
+                .build());
+
+        return "redirect:/admin/board";
     }
 }
