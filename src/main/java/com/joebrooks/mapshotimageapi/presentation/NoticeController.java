fdb3823f0e7c8f126589eabfa1d5e730de5cc18e
joebrooks <lcw3176@naver.com>
2022-04-11 17:01:26 +0900

    공지사항 추가

--- src/main/java/com/joebrooks/mapshotimageapi/presentation/NoticeController.java
+++ src/main/java/com/joebrooks/mapshotimageapi/presentation/NoticeController.java
@@ -1,15 +1,44 @@
 package com.joebrooks.mapshotimageapi.presentation;
 
+import com.joebrooks.mapshotimageapi.repository.notice.NoticeEntity;
+import com.joebrooks.mapshotimageapi.repository.notice.NoticeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/notice")
 public class NoticeController {
 
+    private final NoticeService noticeService;
+
     @GetMapping
-    public String showNoticeList(){
+    public String showNoticeList(@RequestParam(name = "post", required = false) Optional<String> post,
+                                 Model model){
+
+        post.ifPresent((postNumber) -> {
+            for(char i : postNumber.toCharArray()){
+                if(!Character.isDigit(i)){
+                    return;
+                }
+            }
+
+            NoticeEntity noticeEntity = noticeService.getPost(Long.parseLong(postNumber))
+                    .orElseThrow(() -> {
+                        throw new IllegalStateException("잘못된 post 접근");
+                    });
+
+            model.addAttribute("post", noticeEntity);
+        });
+
+        model.addAttribute("posts", noticeService.getPosts(0));
+
         return "notice";
     }
 }
