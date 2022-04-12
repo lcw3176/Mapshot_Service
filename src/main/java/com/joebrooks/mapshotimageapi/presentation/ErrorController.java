925eaa068851b0b4e1fd4c89919ca7dd53000ee0
joebrooks <lcw3176@naver.com>
2022-04-12 12:58:56 +0900

    에러페이지

---
+++ src/main/java/com/joebrooks/mapshotimageapi/presentation/ErrorController.java
@@ -0,0 +1,15 @@
+package com.joebrooks.mapshotimageapi.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error-page")
+public class ErrorController {
+
+    @GetMapping
+    public String showErrorForm(){
+        return "error-page";
+    }
+}
