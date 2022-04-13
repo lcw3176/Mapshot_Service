6c6af5f5633a359550042f40771c82c1b73b0cd5
joebrooks <lcw3176@naver.com>
2022-04-13 14:05:41 +0900

    카카오맵 웹소켓으로 변경중

--- src/main/java/com/joebrooks/mapshotimageapi/kakaoMap/KakaoMapHandler.java
+++ src/main/java/com/joebrooks/mapshotimageapi/kakaoMap/KakaoMapHandler.java
@@ -1,33 +1,78 @@
 package com.joebrooks.mapshotimageapi.kakaoMap;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.LinkedList;
-import java.util.Queue;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class KakaoMapHandler extends TextWebSocketHandler {
 
-    private static Queue<WebSocketSession> waitQueue = new LinkedList<>();
+    private static final LinkedList<WebSocketSession> waitList = new LinkedList<>();
+
+    private final KakaoMapService kakaoMapService;
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
-        waitQueue.add(session);
+        waitList.add(session);
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        if(kakaoMapService.isAvailable() && waitList.size() >= 1){
+            WebSocketSession currentUser = waitList.get(0);
 
+            if(currentUser.equals(session)){
+                try{
+                    KakaoMapResponse waitEndResponse = KakaoMapResponse.builder()
+                            .isMyTurn(true)
+                            .build();
+
+                    session.sendMessage(new TextMessage(mapper.writeValueAsString(waitEndResponse))); // 처리 중 메세지 보내주기
+
+                    KakaoMapRequest request = mapper.readValue(message.getPayload(), KakaoMapRequest.class);
+                    byte[] data = kakaoMapService.getImage(request.getUri());
+
+                    KakaoMapResponse imageResponse = KakaoMapResponse.builder()
+                            .imageComplete(true)
+                            .imageData(data)
+                            .build();
+
+                    currentUser.sendMessage(new TextMessage(mapper.writeValueAsString(imageResponse))); // 이미지 데이터 보내주기
+
+                    return;
+                } catch (Exception e){
+                    log.info("카카오 맵 예외");
+                    log.info(e.getMessage());
+                } finally {
+                    waitList.remove(session);
+                }
+
+            }
+        }
+
+        KakaoMapResponse response = KakaoMapResponse.builder()
+                                        .isMyTurn(false)
+                                        .leftCount(waitList.indexOf(session) + 1)
+                                        .build();
+
+        session.sendMessage(new TextMessage(mapper.writeValueAsString(response))); // 대기 인원 수 보내주기
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-        waitQueue.remove(session);
+        waitList.remove(session);
     }
 }
