1057565e73e04f9517b4b483f2063debfeeb0861
Chanwoo Lee <lcw3176@naver.com>
2022-04-23 02:21:13 +0900

    fix/웹소켓_url_변경 (#39)

--- src/main/java/com/joebrooks/mapshotimageapi/map/websocket/WebSocketConfig.java
+++ src/main/java/com/joebrooks/mapshotimageapi/map/websocket/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(userSocketHandler, "/map/gen")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("https://richshrimp.tk")
                 .withSockJS();
     }
 }
