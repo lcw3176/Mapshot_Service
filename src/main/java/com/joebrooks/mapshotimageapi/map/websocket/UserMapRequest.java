3ab02ad96cb10981c1feff43f688f2f7b1bed7fd
joebrooks <lcw3176@naver.com>
2022-04-23 15:40:18 +0900

    상대경로 처리

--- src/main/java/com/joebrooks/mapshotimageapi/map/websocket/UserMapRequest.java
+++ src/main/java/com/joebrooks/mapshotimageapi/map/websocket/UserMapRequest.java
@@ -27,7 +27,7 @@ public class UserMapRequest {
     public UriComponents getUri(){
 
         return UriComponentsBuilder
-                .fromPath("https://richshrimp.tk/map/gen/" + this.companyType.getType())
+                .fromPath("/map/gen/" + this.companyType.getType())
                 .queryParam("layerMode", this.layerMode)
                 .queryParam("lat", this.lat)
                 .queryParam("lng", this.lng)
