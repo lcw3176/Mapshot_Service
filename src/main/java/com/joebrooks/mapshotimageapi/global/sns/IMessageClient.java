dc5a97c82b7646a0d72d7a419642a99b250a6741
lcw3176 <lcw3176@naver.com>
2022-04-23 21:48:52 +0900

    인터페이스 제거

--- src/main/java/com/joebrooks/mapshotimageapi/global/sns/IMessageClient.java
+++
@@ -1,7 +0,0 @@
-package com.joebrooks.mapshotimageapi.global.sns;
-
-import com.joebrooks.mapshotimageapi.global.exception.ExceptionResponse;
-
-public interface IMessageClient {
-    void sendMessage(ExceptionResponse response);
-}
