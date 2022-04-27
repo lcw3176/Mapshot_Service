7256fe63246ea6a33692bb39e802f64cdda08d72
Chanwoo Lee <lcw3176@naver.com>
2022-04-27 14:25:17 +0900

    Feature/was 변경 (#55)
    * 언더토우 -> 톰캣 변경
    
    * 비동기로 설정 롤백

--- src/main/java/com/joebrooks/mapshotimageapi/map/AsyncConfig.java
+++ src/main/java/com/joebrooks/mapshotimageapi/map/AsyncConfig.java
@@ -7,18 +7,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
-//@Configuration
-//@EnableAsync
-//public class AsyncConfig extends AsyncConfigurerSupport {
-//
-//    @Override
-//    public Executor getAsyncExecutor() {
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        executor.setCorePoolSize(1);
-//        executor.setMaxPoolSize(1);
-//        executor.setQueueCapacity(50);
-//        executor.setThreadNamePrefix("CAPTURE-ASYNC-");
-//        executor.initialize();
-//        return executor;
-//    }
-//}
+@Configuration
+@EnableAsync
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("CAPTURE-ASYNC-");
+        executor.initialize();
+        return executor;
+    }
+}
