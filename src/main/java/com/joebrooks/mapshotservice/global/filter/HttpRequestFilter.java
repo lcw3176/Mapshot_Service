15aafd97c2d93a04c2c2ec0c400d4f311359ff61
Chanwoo Lee <lcw3176@naver.com>
2022-06-08 22:58:50 +0900

    Fix/필터중복으로인한 admin 접근불가 (#85)
    * 필터 조건 추가
    
    * 필터 목록 추가

--- src/main/java/com/joebrooks/mapshotservice/global/filter/HttpRequestFilter.java
+++ src/main/java/com/joebrooks/mapshotservice/global/filter/HttpRequestFilter.java
@@ -1,5 +1,7 @@
 package com.joebrooks.mapshotservice.global.filter;
 
+import org.springframework.util.PatternMatchUtils;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,26 +9,36 @@ import java.io.IOException;
 
 public class HttpRequestFilter implements Filter {
 
+    private static final String[] whitelist = {"/admin/login", "/admin/board/register", "/admin/board/edit", "/favicon.ico"};
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String requestURI = httpRequest.getRequestURI();
 
+        if(isNotWhitelistPath(requestURI)){
+            if (httpRequest.getMethod().equalsIgnoreCase("OPTIONS")
+                    || httpRequest.getMethod().equalsIgnoreCase("PUT")
+                    || httpRequest.getMethod().equalsIgnoreCase("DELETE")
+                    || httpRequest.getMethod().equalsIgnoreCase("TRACE")
+                    || httpRequest.getMethod().equalsIgnoreCase("PATCH")
+                    || httpRequest.getMethod().equalsIgnoreCase("OPTION")
+                    || httpRequest.getMethod().equalsIgnoreCase("POST")) {
 
-        if (httpRequest.getMethod().equalsIgnoreCase("OPTIONS")
-                || httpRequest.getMethod().equalsIgnoreCase("PUT")
-                || httpRequest.getMethod().equalsIgnoreCase("DELETE")
-                || httpRequest.getMethod().equalsIgnoreCase("TRACE")
-                || httpRequest.getMethod().equalsIgnoreCase("PATCH")
-                || httpRequest.getMethod().equalsIgnoreCase("OPTION")
-                || httpRequest.getMethod().equalsIgnoreCase("POST")) {
+                httpResponse.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                httpResponse.setStatus(405);
 
-            httpResponse.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-            httpResponse.setStatus(405);
-
-            return;
+                return;
+            }
         }
+
 
         chain.doFilter(request, response);
     }
+
+    private boolean isNotWhitelistPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
+
 }
