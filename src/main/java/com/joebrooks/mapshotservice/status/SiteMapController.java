b2eb73242669e1ce78d2d5b065e9218108d69d0d
Chanwoo Lee <lcw3176@naver.com>
2022-06-27 01:10:42 +0900

    Feature/GitHub action (#89)
    * 배포 스크립트
    
    * 사이트맵 비활성화

--- src/main/java/com/joebrooks/mapshotservice/status/SiteMapController.java
+++ src/main/java/com/joebrooks/mapshotservice/status/SiteMapController.java
@@ -1,31 +1,31 @@
-package com.joebrooks.mapshotservice.status;
-
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.ContentDisposition;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.File;
-
-@RestController
-@RequestMapping("/status/sitemap")
-public class SiteMapController {
-
-    @GetMapping
-    public ResponseEntity<FileSystemResource> returnSiteMap() {
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setContentDisposition(ContentDisposition
-                .builder("attachment")
-                .filename("sitemap.xml")
-                .build());
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(new FileSystemResource(new File("sitemap.xml")));
-    }
-}
+//package com.joebrooks.mapshotservice.status;
+//
+//import org.springframework.core.io.FileSystemResource;
+//import org.springframework.http.ContentDisposition;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.File;
+//
+//@RestController
+//@RequestMapping("/status/sitemap")
+//public class SiteMapController {
+//
+//    @GetMapping
+//    public ResponseEntity<FileSystemResource> returnSiteMap() {
+//        HttpHeaders headers = new HttpHeaders();
+//
+//        headers.setContentDisposition(ContentDisposition
+//                .builder("attachment")
+//                .filename("sitemap.xml")
+//                .build());
+//
+//        return ResponseEntity.ok()
+//                .headers(headers)
+//                .body(new FileSystemResource(new File("sitemap.xml")));
+//    }
+//}
