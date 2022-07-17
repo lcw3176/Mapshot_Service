c8527d53fe4d62e15a1764da2b15ad7301d10c90
ChanwooLee <lcw3176@naver.com>
2022-07-17 13:22:39 +0900

    예외 처리 수정중

--- src/main/java/com/joebrooks/mapshotservice/global/exception/RestControllerExceptionAdvice.java
+++ src/main/java/com/joebrooks/mapshotservice/global/exception/RestControllerExceptionAdvice.java
@@ -3,7 +3,9 @@ package com.joebrooks.mapshotservice.global.exception;
 import com.joebrooks.mapshotservice.global.sns.SlackClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -14,6 +16,7 @@ public class RestControllerExceptionAdvice {
     private final SlackClient slackClient;
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public void exceptionHandler(Exception e) {
         log.error(e.getMessage(), e);
         slackClient.sendMessage(e);
