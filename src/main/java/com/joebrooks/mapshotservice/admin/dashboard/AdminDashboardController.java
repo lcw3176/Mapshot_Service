85d7aa9e0a5bba2472ea21b6c31aa7e1556a74a7
lcw3176 <lcw3176@naver.com>
2022-11-27 00:35:54 +0900

    페이지 처리 및 뷰 개선

--- src/main/java/com/joebrooks/mapshotservice/admin/dashboard/AdminDashboardController.java
+++ src/main/java/com/joebrooks/mapshotservice/admin/dashboard/AdminDashboardController.java
@@ -1,9 +1,12 @@
 package com.joebrooks.mapshotservice.admin.dashboard;
 
 import com.joebrooks.mapshotservice.global.util.PageGenerator;
+import com.joebrooks.mapshotservice.notice.NoticeEntity;
 import com.joebrooks.mapshotservice.notice.NoticeService;
 import com.joebrooks.mapshotservice.notice.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,25 +21,29 @@ import javax.validation.constraints.Positive;
 public class AdminDashboardController {
 
     private final NoticeService noticeService;
+    private static final int OFFSET = 1;
 
     @GetMapping
-    public String showAdminPage(@Positive @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+    public String showAdminPage(@Positive @RequestParam(name = "page", required = false, defaultValue = "1") int requestPage,
                                 Model model){
 
-        if(!PageGenerator.isValidate(page, noticeService.getSize())){
-            page = 1;
-        }
+        int index = requestPage - OFFSET;
+        Page<NoticeEntity> pages =  noticeService.getPosts(index);
 
-        int startPage = PageGenerator.getStartPage(page);
-        int lastPage = PageGenerator.getLastPage(page, noticeService.getSize());
+        int startPage = PageGenerator.getStartPage(requestPage);
+        int lastPage = PageGenerator.getLastPage(requestPage, pages.getTotalElements());
 
-        model.addAttribute("posts", noticeService.getPosts(page - 1));
+        Pageable pageable = pages.getPageable();
+
+        model.addAttribute("posts", pages);
         model.addAttribute("pageResponse", PageResponse.builder()
                 .startPage(startPage)
                 .lastPage(lastPage)
-                .nowPage(page)
-                .nextPage(PageGenerator.getNextPage(lastPage, noticeService.getSize()))
-                .previousPage(PageGenerator.getPreviousPage(startPage))
+                .nowPage(requestPage)
+                .previousPage(pageable.previousOrFirst().getPageNumber() + OFFSET)
+                .nextPage(pageable.next().getPageNumber() + OFFSET)
+                .hasNext(pages.hasNext())
+                .hasPrevious(pages.hasPrevious())
                 .build());
 
         return "fragment/admin/admin-board";
