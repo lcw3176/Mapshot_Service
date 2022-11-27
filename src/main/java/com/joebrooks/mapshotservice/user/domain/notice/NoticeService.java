3dd607f018b4cd46a56d781b138818ae4cac4665
lcw3176 <lcw3176@naver.com>
2022-11-27 14:48:27 +0900

    refactor(AdminService, NoticeService) : Transactional readonly 설정 추가

--- src/main/java/com/joebrooks/mapshotservice/user/domain/notice/NoticeService.java
+++ src/main/java/com/joebrooks/mapshotservice/user/domain/notice/NoticeService.java
@@ -8,31 +8,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class NoticeService {
 
     private final NoticeRepository noticeRepository;
     private static final int PAGE_SIZE = 10;
 
+    @Transactional
     public void addPost(NoticeEntity noticeEntity){
         noticeRepository.save(noticeEntity);
     }
 
+    @Transactional(readOnly = true)
     public Page<NoticeEntity> getPosts(int index){
         return noticeRepository.findAll(PageRequest.of(index, PAGE_SIZE, Sort.by("id").descending()));
     }
 
+    @Transactional(readOnly = true)
     public NoticeEntity getPost(long id){
         return noticeRepository.findById(id).orElseThrow(() -> {
             throw new IllegalArgumentException("잘못된 공지사항 접근");
         });
     }
 
+    @Transactional
     public void removePost(long id){
         noticeRepository.deleteById(id);
     }
 
+    @Transactional
     public void editPost(NoticeEntity noticeEntityToChange){
 
         NoticeEntity noticeEntity = noticeRepository.findById(noticeEntityToChange.getId())
