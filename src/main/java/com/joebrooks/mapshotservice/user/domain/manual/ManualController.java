e3f4e06effa414b84ca06dfaed16540591420be3
Chanwoo Lee <lcw3176@naver.com>
2022-12-04 21:18:22 +0900

    Refactor/메뉴얼 개편 (#110)
    * fix: 구글 관련 내용 제거
    
    * chore(index.html): 불필요한 문구 정리
    
    * refactor(manual): 싱글 페이지로 동작하게 변경

--- src/main/java/com/joebrooks/mapshotservice/user/domain/manual/ManualController.java
+++ src/main/java/com/joebrooks/mapshotservice/user/domain/manual/ManualController.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.constraints.PositiveOrZero;
 
 @Controller
 @Validated
@@ -15,19 +12,8 @@ import javax.validation.constraints.PositiveOrZero;
 public class ManualController {
 
     @GetMapping
-    public String setPageAndRedirect(){
-
-        return "redirect:/manual/0";
-    }
-
-    @GetMapping("/{page}")
-    public String showManual(Model model, @PositiveOrZero @PathVariable(value = "page", required = false) Integer page){
-        if(page >= ManualType.values().length){
-            throw new IllegalArgumentException("잘못된 매뉴얼 접근");
-        }
-
+    public String showManual(Model model){
         model.addAttribute("manual", ManualType.values());
-        model.addAttribute("pageNumber", page);
 
         return "fragment/manual/manual";
     }
